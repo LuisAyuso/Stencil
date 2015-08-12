5d6013ef83e484f6e31fb574b49bb1ad71895697
Luis Ayuso <luis@dps.uibk.ac.at>
2015-08-12 18:45:28 +0200

    benchmark skogafoss

--- benchmark.sh
+++ benchmark.sh
@@ -15,36 +15,43 @@ then
 	exit -1
 fi
 
-echo "algorithm;time;cache-misses;L1-dcache-load-misses;L1-dcache-store-misses"
+echo "algorithm;time-steps;time;cache-misses;L1-dcache-load-misses;L1-dcache-store-misses"
 
 for REPETITIONS in `seq 1 10`
 do
 
-	for TIMESTEPS in 1 10 100 1000 
+	for TIMESTEPS in 100 1000 5000 10000
 	do
+		./Stencil -i $1 -r $TIMESTEPS > out 
 
-		perf stat -e cache-misses,L1-dcache-load-misses,L1-dcache-store-misses -x ";" ./Stencil -i $1 -r $TIMESTEPS rec > out 2> perf
+		if ! grep -q "VALIDATION OK" out;
+		then
+			echo "validation failed at: " $TIMESTEPS
+			exit 1
+		fi
+
+		perf stat -e cache-misses,L1-dcache-load-misses,L1-dcache-store-misses -x ";" taskset 0x4 ./Stencil -i $1 -r $TIMESTEPS rec > out 2> perf
 		CACHE_MISES=`grep "cache-misses" perf | cut -f 1 -d ";"`
 		L1_LOAD_MISES=`grep "L1-dcache-load-misses" perf | cut -f 1 -d ";"`
 		L1_STORE_MISSES=`grep "L1-dcache-store-misses" perf | cut -f 1 -d ";"`
-		LINE=`grep "ms" out | cut -f 2 -d " " | cut -f 1 -d "m"`
-		LINE="rec;"$TIMESTEPS";"$LINE";"$CACHE_MISES";"$L1_LOAD_MISES";"$L1_STORE_MISSES
+		TIME=`grep "ms" out | cut -f 2 -d " " | cut -f 1 -d "m"`
+		LINE="rec;"$TIMESTEPS";"$TIME";"$CACHE_MISES";"$L1_LOAD_MISES";"$L1_STORE_MISSES
 		echo $LINE
 
-		perf stat -e cache-misses,L1-dcache-load-misses,L1-dcache-store-misses -x ";" ./Stencil -i $1 -r $TIMESTEPS it > out 2> perf
+		perf stat -e cache-misses,L1-dcache-load-misses,L1-dcache-store-misses -x ";" taskset 0x4 ./Stencil -i $1 -r $TIMESTEPS it > out 2> perf
 		CACHE_MISES=`grep "cache-misses" perf | cut -f 1 -d ";"`
 		L1_LOAD_MISES=`grep "L1-dcache-load-misses" perf | cut -f 1 -d ";"`
 		L1_STORE_MISSES=`grep "L1-dcache-store-misses" perf | cut -f 1 -d ";"`
-		LINE=`grep "ms" out | cut -f 2 -d " " | cut -f 1 -d "m"`
-		LINE="it;"$TIMESTEPS";"$LINE";"$CACHE_MISES";"$L1_LOAD_MISES";"$L1_STORE_MISSES
+		TIME=`grep "ms" out | cut -f 2 -d " " | cut -f 1 -d "m"`
+		LINE="it;"$TIMESTEPS";"$TIME";"$CACHE_MISES";"$L1_LOAD_MISES";"$L1_STORE_MISSES
 		echo $LINE
 
-		perf stat -e cache-misses,L1-dcache-load-misses,L1-dcache-store-misses -x ";" ./Stencil -i $1 -r $TIMESTEPS inv > out 2> perf
+		perf stat -e cache-misses,L1-dcache-load-misses,L1-dcache-store-misses -x ";" taskset 0x4 ./Stencil -i $1 -r $TIMESTEPS inv > out 2> perf
 		CACHE_MISES=`grep "cache-misses" perf | cut -f 1 -d ";"`
 		L1_LOAD_MISES=`grep "L1-dcache-load-misses" perf | cut -f 1 -d ";"`
 		L1_STORE_MISSES=`grep "L1-dcache-store-misses" perf | cut -f 1 -d ";"`
-		LINE=`grep "ms" out | cut -f 2 -d " " | cut -f 1 -d "m"`
-		LINE="inv;"$TIMESTEPS";"$LINE";"$CACHE_MISES";"$L1_LOAD_MISES";"$L1_STORE_MISSES
+		TIME=`grep "ms" out | cut -f 2 -d " " | cut -f 1 -d "m"`
+		LINE="inv;"$TIMESTEPS";"$TIME";"$CACHE_MISES";"$L1_LOAD_MISES";"$L1_STORE_MISSES
 		echo $LINE
 
 	done
