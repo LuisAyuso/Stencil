b6bcd000209708c3f9490b4160288bfd787adc97
Luis Ayuso <luis@dps.uibk.ac.at>
2015-08-13 13:29:24 +0200

    get cimg from the web'

--- cmake/cimg.cmake
+++ cmake/cimg.cmake
@@ -8,8 +8,9 @@ SET_DIRECTORY_PROPERTIES(PROPERTIES EP_PREFIX ${CMAKE_BINARY_DIR}/third_party/ci
 
 # Add CImg https://github.com/dtschump/CImg.git
 ExternalProject_Add(
-    cimg
-    GIT_REPOSITORY https://github.com/dtschump/CImg.git
+    cimg_download
+	URL http://cimg.eu/files/CImg_latest.zip
+    #GIT_REPOSITORY https://github.com/dtschump/CImg.git
     # TIMEOUT 10
     # do not configure
     CONFIGURE_COMMAND  ""
@@ -17,9 +18,24 @@ ExternalProject_Add(
     BUILD_COMMAND  ""
     # Disable install step
     INSTALL_COMMAND ""
-    )
+    LOG_DOWNLOAD OFF
+    LOG_CONFIGURE OFF
+    LOG_BUILD OFF
+)
+
+
 
 # Specify include dir
-ExternalProject_Get_Property(cimg source_dir)
-set(CIMG_INCLUDE_DIR ${source_dir})
+ExternalProject_Get_Property(cimg_download source_dir)
+set(CIMG_DIR ${source_dir})
+
+
+add_custom_target( cimg DEPENDS cimg_download)
+set_property(TARGET cimg PROPERTY header_dir ${CIMG_DIR})
+get_property(CIMG_INCLUDE_DIR TARGET cimg PROPERTY header_dir)
+
+
+
+
+
 
