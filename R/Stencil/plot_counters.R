5fc81903a29a8c7285785cdb0ca3679d05dddb84
Luis Ayuso <luis@dps.uibk.ac.at>
2015-08-17 10:49:10 +0200

    R plot routines

--- R/Stencil/plot_counters.R
+++ R/Stencil/plot_counters.R
@@ -12,3 +12,15 @@ plot_measure <- function(data, measure){
   
   plot + facet_grid( ~ time.steps) + geom_boxplot(aes_string(x="class", y=measure   )) +scale_y_log10() 
 }
+
+
+plot_input_size_vs_cores <- function(data, measure){
+  plot <- ggplot(data)
+  plot + geom_boxplot(aes_string(x="algorithm", y=measure)) + facet_grid( input.size ~ num.cores)
+}
+
+
+plot_bars_avg <- function( data, measure){
+
+ #meanMes <- tapply(data[measure], diamonds$cut, mean)
+}
