33ebce17aa85e32231832c0b92e7afb0513b9aa8
Luis Ayuso <luis@dps.uibk.ac.at>
2015-08-18 13:13:05 +0200

    thread pool, not working, try insieme RT

--- include/dispatch.h
+++ include/dispatch.h
@@ -120,12 +120,123 @@
 		std::atomic_long current_threads (0);
 
 
+		class Thread_Pool{
+
+			typedef std::pair<std::function<void(void)>, std::promise<void>> task_t;
+
+			struct Thread_queue{
+
+				std::vector<task_t> task_queue;
+				std::mutex queue_lock;
+				int next_task;
+				bool go;
+
+				Thread_queue()
+				: next_task(0), go(true) {}
+			};
+
+			const int num_threads;
+			std::vector<Thread_queue> thread_queues;
+			std::atomic_long next_thread;
+			std::vector<std::thread> thread_pids;
+			bool running;
+
+
+			static void run (Thread_queue& queue){
+
+				auto task_available = [&] () { 
+					queue.queue_lock.lock();
+					auto nelem =  queue.task_queue.size();
+					queue.queue_lock.unlock();
+					return queue.next_task < nelem;
+				};
+
+				while (queue.go){
+
+					while(task_available()){
+
+						std::cout << "execute: " << queue.next_task << " of " << queue.task_queue.size() << std::endl;
+
+						// execute task
+						queue.task_queue[queue.next_task].first();
+						// make future ready
+						queue.task_queue[queue.next_task].second.set_value();
+						queue.next_task ++;
+
+					}
+			//		std::cout << "list empty" << std::endl;
+
+					std::this_thread::yield();
+				}
+				
+			}
+
+
+			Thread_Pool()
+			: num_threads(std::thread::hardware_concurrency()), thread_queues(num_threads), next_thread(0), thread_pids(num_threads), running(false)
+			{ 
+			}
+
+			void _init(){
+				for (int i =0; i < num_threads; ++i){
+
+					thread_queues[i].go = true;
+					thread_queues[i].next_task = 0;
+
+					std::thread th (Thread_Pool::run, std::ref(thread_queues[i]));
+					thread_pids.emplace(thread_pids.begin()+i, std::move(th) );
+				}
+				running = true;
+			}
+			void _shutdown(){
+				for (int i =0; i < num_threads; ++i){
+					thread_queues[i].go = false;
+				}
+				for (int i =0; i < num_threads; ++i){
+					thread_pids[i].join();
+					thread_queues[i].task_queue.clear();
+				}
+
+				running = false;
+			}
+
+
+			std::future<void> _add_task(std::function<void(void)> f) {
+				int current = next_thread.fetch_add(1) % num_threads;
+				thread_queues[current].queue_lock.lock();
+				thread_queues[current].task_queue.push_back({f, std::promise<void>()});
+				auto fut = thread_queues[current].task_queue.back().second.get_future();
+				thread_queues[current].queue_lock.unlock();
+				std::cout << "task added to: " << current<<std::endl;
+				return fut;
+			}
+
+		public:
+
+			static Thread_Pool& get_instance(){
+				static Thread_Pool inst;
+				if (!inst.running) inst._init();
+				return inst;
+			}
+
+			static std::future<void> add_task(std::function<void(void)> f) {
+				return get_instance()._add_task(f);
+			}
+
+			static void shutdown() {
+				get_instance()._shutdown();
+			}
+
+		};
+
+
 		std::future<void> my_async(std::function<void(void)> f){
 			if (current_threads < max_threads * THREAD_CUTOFF) {
 				current_threads++; 
 				auto wrap = [f]() { f(); current_threads--;};
 //				std::cout << "Async call " << current_threads << std::endl;
 				return std::async(std::launch::async, wrap);
+//				return Thread_Pool::add_task(wrap);
 			} 
 			else{ 
 //				std::cout << "Deferred call " << current_threads << std::endl;
@@ -136,9 +247,10 @@
 		std::future<void> my_async(std::function<void(int)> f, int arg){
 			if (current_threads < max_threads * THREAD_CUTOFF) {
 				current_threads++; 
-				auto wrap = [f] (int arg) { f(arg); current_threads--;};
+				auto wrap = [=] () { f(arg); current_threads--;};
 //				std::cout << "Async call " << current_threads << std::endl;
-				return std::async(std::launch::async, wrap, arg);
+				return std::async(std::launch::async, wrap);
+//				return Thread_Pool::add_task(wrap);
 			} 
 			else{ 
 //				std::cout << "Deferred call " << current_threads << std::endl;
@@ -162,11 +274,11 @@
 
 	#define P_FOR(it, B, E, S, STMT) \
 		{ \
-			std::vector<std::future<void>> promises; \
+			std::vector<std::future<void>> futures; \
 			auto wrap = [&] (int it) { STMT; }; \
 			for (auto it = B; it < E; it += S) \
-				promises.push_back(my_async(wrap, it)); \
-			for (const auto& f : promises) f.wait(); \
+				futures.push_back(my_async(wrap, it)); \
+			for (const auto& f : futures) f.wait(); \
 		}
 
 
